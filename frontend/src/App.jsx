94c6562b5050047565b41d3004e5cd60a894876a
Shubhajit <shubjitgithub@gmail.com>
2025-01-10 10:14:18 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+//App.jsx
+
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Signin } from './pages/Signin'
+import { Blog } from './pages/Blog'
+import { Blogs } from './pages/Blogs'
+import { Signup } from './pages/Signup'
+import Publish from './pages/Publish';
 
 function App() {
-  return (
-    <div>App</div>
-  )
+
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/signin" element={<Signin />} />
+                    <Route path="/blog/:id" element={<Blog />} />
+                    <Route path="/blogs" element={<Blogs />} />
+                    <Route path="/publish" element={<Publish />}></Route>
+                    <Route path="" element={<Signin />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
